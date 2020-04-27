faf3d03f9a4c383d747b8d514cdd775adbb20f41
Abe Augustijn <abe.augustijn@hotmail.nl>
2020-04-27 14:39:05 +0200

    Fix: stdin

--- replace_header.sh
+++ replace_header.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
 
-if [ -z "$1" ]
-then
+usage() {
 	echo "Usage: ./replace_header.sh [filename]"
-	exit 1
+	echo "Reading a list filenames from stdin is also possible"
+}
+
+replace() {
+	if ! [ -f $1 ]; then
+		echo "Invalid file: $1"
+		return
+	fi
+
+	sed -i 's/        :::      ::::::::/        ::::::::         /g' $1
+	sed -i 's/      :+:      :+:    :+:/      :+:    :+:         /g' $1
+	sed -i 's/    +:+ +:+         +:+  /     +:+                 /g' $1
+	sed -i 's/  +#+  +:+       +#+     /    +#+                  /g' $1
+	sed -i 's/+#+#+#+#+#+   +#+        /   +#+                   /g' $1
+	sed -i 's/     #+#    #+#          / #+#   #+#               /g' $1
+	sed -i 's/    ###   ########.fr    / ########   odam.nl      /g' $1
+}
+
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+	usage
+	exit 0
 fi
 
-if ! [ -f $1 ]; then
-	echo "Invalid file: $1"
-	exit 1
+if [ "$1" == "" ]
+then
+	while read line
+	do
+		replace "$line"
+	done < "/dev/stdin"
+else
+	replace "$1"
 fi
-
-sed -i 's/        :::      ::::::::/        ::::::::         /g' $1
-sed -i 's/      :+:      :+:    :+:/      :+:    :+:         /g' $1
-sed -i 's/    +:+ +:+         +:+  /     +:+                 /g' $1
-sed -i 's/  +#+  +:+       +#+     /    +#+                  /g' $1
-sed -i 's/+#+#+#+#+#+   +#+        /   +#+                   /g' $1
-sed -i 's/     #+#    #+#          / #+#   #+#               /g' $1
-sed -i 's/    ###   ########.fr    / ########   odam.nl      /g' $1
